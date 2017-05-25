d1ffc2f16595a7f5570306ba38dff7298e930cd3
FPierre <pflauder@gmail.com>
2017-05-25 22:25:01 +0200

    WIP reponse du serveur en GraphQL

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const graphql = require('graphql')
+const schema = require('./schema')
+
 const app = express()
+app.use(bodyParser.text({ type: 'application/graphql' }))
 
 app.post('/endpoint', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-  res.json({ foo: 'bar' })
+
+  graphql(schema, req.body).then(result => {
+    res.send(JSON.stringify(result, null, 2))
+  })
 })
 
 app.listen(3003)
